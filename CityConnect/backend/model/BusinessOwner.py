20421868997b2357c1160a0edb8b16b1d808447a
fishbrook <mh6009@nyu.edu>
2024-03-26 16:09:30 +0800

    bo and p

---
+++ CityConnect/backend/model/BusinessOwner.py
@@ -0,0 +1,48 @@
+import mysql.connector
+from django.db import models
+from django.utils import timezone
+from .models import Place
+
+class BusinessOwner(models.Model):
+    phone = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    
+
+    def Search_Place(self, name, city=self.city):
+        # change order by distance
+        return Place.objects.filter(Q(name=name) & Q(city=city)).order_by("-creation_date")
+
+    def Search_Event(self, name, city=self.city, Etype, NotFull=True):
+        # change order by distance
+        return Event.objects.filter(Q(name=name) & Q(city=city) & Q(event_type=Etype) & Q(Check_Full=full)).order_by("-creation_date")
+
+    def Post_Event(self, name, start_time, end_time, description=None, max_ppl, event_type, Place):
+        return Event.objects.create(
+            name = name,
+            business_owner = self,
+            start_time = start_time,
+            end_time = end_time,
+            description = description,
+            max_ppl = max_ppl, 
+            current_ppl = 0,
+            score = 5,
+            avg_price = 0,
+            event_type = event_type,
+            place = Place,
+        )
+    
+    def Delete_Event(self, Event):
+        if Event.business_owner == self:
+            Event.delete()
+
+    def Get_Phone(self):
+        return self.phone
+
+    def Get_City(self):
+        return self.city
+
+    def Set_Phone(self, phone):
+        self.phone = phone
+    
+    def Set_City(self, city):
+        self.city = city
--- CityConnect/backend/model/place.py
+++ CityConnect/backend/model/place.py
@@ -1,101 +1,86 @@
 import mysql.connector
+from django.db import models
+from django.utils import timezone
 
-class Place:
-    def __init__(self, host, user, password, database):
-        self.connection = mysql.connector.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=database
-        )
-        self.name = None
-        self.longitude = None # location_longtitude
-        self.latitude = None # location_latitude
-        self.city = None
+class Place(models.Model):
+    # def __init__(self, host, user, password, database):
+        # self.connection = mysql.connector.connect(
+        #     host=host,
+        #     user=user,
+        #     password=password,
+        #     database=database
+        # )
+        # self.name = None
+        # self.longitude = None
+        # self.latitude = None
+        # self.city = None
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    city = models.CharField(max_length=50)
 
-    def Get_Name(self, longitude, latitude):
-        cursor = self.connection.cursor()
-        query = """
-        SELECT name FROM place 
-        WHERE longitude = %s AND latitude = %s
-        """
-        cursor.execute(query, (longitude, latitude))
-        result = cursor.fetchone()
-        cursor.close()
-        if result:
-            return result[0]
-        else:
-            return "No place found"
-        
-    def Get_location_longitude(self, name):
-        cursor = self.connection.cursor()
-        query = "SELECT longitude FROM place WHERE name = %s"
-        cursor.execute(query, (name,))
-        result = cursor.fetchone()
-        cursor.close()
-        if result:
-            return result[0]
-        else:
-            return "No place found"
+    # def Get_Name(self):
+    #     return self.name
+    def Get_Name(self):
+        return self.name
+    # def Get_Name(self, longitude, latitude):
+    #     cursor = self.connection.cursor()
+    #     query = """
+    #     SELECT name FROM place 
+    #     WHERE longitude = %s AND latitude = %s
+    #     """
+    #     cursor.execute(query, (longitude, latitude))
+    #     result = cursor.fetchone()
+    #     cursor.close()
+    #     if result:
+    #         return result[0]
+    #     else:
+    #         return "No place found with the provided coordinates."
 
-    def Get_location_latitude(self, name):
-        cursor = self.connection.cursor()
-        query = "SELECT latitude FROM place WHERE name = %s"
-        cursor.execute(query, (name,))
-        result = cursor.fetchone()
-        cursor.close()
-        if result:
-            return result[0]
-        else:
-            return "No place found"
+    # def Get_location_longitude(self):
+    #     return self.longitude
+    def Get_Longitude(self):
+        return self.longitude
+    # def Get_location_longitude(self, name):
+    #     cursor = self.connection.cursor()
+    #     query = "SELECT longitude FROM place WHERE name = %s"
+    #     cursor.execute(query, (name,))
+    #     result = cursor.fetchone()
+    #     cursor.close()
+    #     if result:
+    #         return result[0]
+    #     else:
+    #         return "No place found with this name."
 
-    def Get_city(self, name):
-        cursor = self.connection.cursor()
-        query = "SELECT city FROM place WHERE name = %s"
-        cursor.execute(query, (name,))
-        result = cursor.fetchone()
-        cursor.close()
-        if result:
-            return result[0]
-        else:
-            return "No place found"
+    def Get_Latitude(self):
+        return self.latitude
 
-    def Set_Name(self, longitude, latitude, new_name):
-        cursor = self.connection.cursor()
-        query = """
-        UPDATE place 
-        SET name = %s 
-        WHERE longitude = %s AND latitude = %s
-        """
-        cursor.execute(query, (new_name, longitude, latitude))
-        self.connection.commit() 
-        affected_rows = cursor.rowcount
-        cursor.close()
-        if affected_rows > 0:
-            return "Name updated successfully."
-        else:
-            return "No place found"
+    def Get_City(self):
+        return self.city
 
-
+    def Set_Name(self, name):
+        # update, input longi&lati, reset name
+        self.name = name
+        # cursor = self.connection.cursor()
+        # query = "SELECT name, longitude, latitude, city FROM places WHERE name = %s"
+        # cursor.execute(query, (name,))
+        # result = cursor.fetchone()
+        # cursor.close()
+        # if result:
+        #     self.name, self.longitude, self.latitude, self.city = result
 
 # Example usage:
 # Initialize the Place object with your MySQL database credentials
 place = Place(host='127.0.0.1', user='root', password='', database='software_engineering')
 
+# Set the name of the place to fetch its details
+# place.Set_Name("Eiffel Tower")
 
 # Access the details
-name = place.Get_Name(longitude='31.2337000000', latitude='121.5050000000')
-print(name)  # Shanghai Tower
+name = place.Get_Name(longitude='48.8583720000', latitude='2.2944810000')
+print(name)  # Eiffel Tower
 
-longitude = place.Get_location_longitude("Shanghai Tower")
-print(longitude) # 31.2337000000
+# longitude = place.Get_location_longitude("Eiffel Tower")
+# print(longitude)
 
-latitude = place.Get_location_latitude("Shanghai Tower")
-print(latitude) # 121.5050000000
-
-city = place.Get_location_latitude("Shanghai Tower")
-print(city) # Shanghai
-
-place.Set_Name(longitude='31.2337000000', latitude='121.5050000000', new_name = 'Test_Update_Shanghai_Tower')
-new_name = place.Get_Name(longitude='31.2337000000', latitude='121.5050000000')
-print(new_name) # Test_Update_Shanghai_Tower
