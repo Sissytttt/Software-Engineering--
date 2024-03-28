44172ce7039af08cc2aec8ce8d295ed860ee88f6
ZiyeJin <zj2092@nyu.edu>
2024-03-28 20:00:13 +0800

    client event follow update

--- CityConnect/backend/dao/models/client.py
+++ CityConnect/backend/dao/models/client.py
@@ -9,7 +9,7 @@ class Client(models.Model):
 
     phone_number = models.IntegerField(primary_key=True)
     city = models.CharField(max_length=100)
-    follows = models.ManyToManyField(related_name='followers', related_query_name='follower', to='dao.client')
+    follows = models.ManyToManyField('self', related_name='followers', related_query_name='follower', to='dao.client')
 
     def Search_Place(self, name, city=city):
         # change order by distance
@@ -23,10 +23,12 @@ class Client(models.Model):
         if event.check_full() == True and event.get_current_ppl() < event.get_max_ppl():
             event.update_current_ppl(event.get_current_ppl()+1)
         # update rsvp many to many table
-        return Event.objects.filter(Q(name=name) & Q(start_time=start_time) & Q(end_time=end_time) & Q(Etype=Etype) & Q(Check_full=full)).order_by("-creation_date")
+        return Event.objects.filter(Q(current_ppl__in=self)).order_by("-creation_date") # return a list of events rsvp by the client
     
-    def Cancel_Event(self):
-        return
+    def Cancel_Event(self, event):
+        if self.events.filter(id=event.id).exists():
+            event.update_current_ppl(event.get_current_ppl() - 1)
+        return Event.objects.filter(Q(current_ppl__in=self)).order_by("-creation_date")
     
     def View_Map(self):
         return
@@ -44,16 +46,18 @@ class Client(models.Model):
         return
     
     def Get_Followers(self):
-        return
+        return self.followers.all()
     
     def Get_Follows(self):
-        return
+        return self.follows.all()
     
-    def Follow(self):
-        return
+    def Follow(self, followee):
+        self.follows.add(followee)
+        self.save()
     
-    def Unfollow(self):
-        return
+    def Unfollow(self, followee):
+        self.follows.remove(followee)
+        self.save()
     
 
 # Example usage:
