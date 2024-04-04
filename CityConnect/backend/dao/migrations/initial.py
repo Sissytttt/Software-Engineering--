036062f90d28410025b5127634538c29f1de8d0e
Sissytttt <st4228@nyu.edu>
2024-04-04 21:50:28 +0800

    Update initial.py

--- CityConnect/backend/dao/migrations/initial.py
+++ CityConnect/backend/dao/migrations/initial.py
@@ -72,7 +72,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='BusinessOwner',
             fields=[
-                ('phone_number', models.IntegerField(primary_key=True)),
+                ('phone_number', models.IntegerField(primary_key=True, max_length=20)),
                 ('city', models.CharField( max_length=100)),
             ],
         ),
